1caa4f4c22c477c14ec22cea3cd927a354f29c32
Jan Mewes <jan.mewes@ks-plus.org>
2023-06-20 06:47:43 +0200

    ci: update javadoc publish script

--- .github/scripts/update_javadocs.sh
+++ .github/scripts/update_javadocs.sh
@@ -4,7 +4,7 @@ set -e
 
 GH_PAGES_DIR=$(mktemp -d)
 
-git clone -b gh-pages --single-branch https://github.com/openehr-java-api/openehr-base-j3x.git ${GH_PAGES_DIR}
+git clone -b gh-pages --single-branch https://github.com/openehr-java-api/openehr-base-4j.git ${GH_PAGES_DIR}
 
 # Remove old javadocs
 if [[ -d $GH_PAGES_DIR/javadocs ]]; then
@@ -15,7 +15,7 @@ mkdir $GH_PAGES_DIR/javadocs
 # Copy new javadocs
 find . -name "*javadoc.jar" | while read FILE
 do
-  SPECIFICATION=$(echo "${FILE}" | perl -pe 's/.\/specifications\/(.*?)\/.*/$1/')
+  SPECIFICATION=$(echo "${FILE}" | perl -pe 's/.\/(.*?)\/.*/$1/')
   unzip "${FILE}" -d "${GH_PAGES_DIR}/javadocs/${SPECIFICATION}"
 done
 
