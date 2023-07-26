0e3c6ceb3c3237969aed62f88c04e6b9b2b6f45b
Jan Mewes <jan.mewes@ks-plus.org>
2023-07-26 16:37:47 +0200

    docs: add javadocs for timezone

--- openehr-base-foundation-types/src/main/java/org/openehr/base/foundation_types/time/Iso8601Timezone.java
+++ openehr-base-foundation-types/src/main/java/org/openehr/base/foundation_types/time/Iso8601Timezone.java
@@ -4,7 +4,34 @@ import java.lang.Boolean;
 import java.lang.String;
 
 /**
- * ISO8601 timezone string, in format:
+ * <div class="content"><div class="paragraph">
+ * <p>ISO8601 timezone string, in format:</p>
+ * </div>
+ * <div class="ulist">
+ * <ul>
+ * <li>
+ * <p><code>Z | ±hh[mm]</code></p>
+ * </li>
+ * </ul>
+ * </div>
+ * <div class="paragraph">
+ * <p>where:</p>
+ * </div>
+ * <div class="ulist">
+ * <ul>
+ * <li>
+ * <p><code>hh</code> is "00" - "23" (0-filled to two digits)</p>
+ * </li>
+ * <li>
+ * <p><code>mm</code> is "00" - "59" (0-filled to two digits)</p>
+ * </li>
+ * <li>
+ * <p><code>Z</code> is a literal meaning UTC (modern replacement for GMT), i.e. timezone <code>+0000</code></p>
+ * </li>
+ * </ul>
+ * </div></div>
+ *
+ * @see <a href="https://specifications.openehr.org/releases/BASE/Release-1.2.0/foundation_types.html#_iso8601_timezone_class">specifications.openehr.org</a>
  */
 public interface Iso8601Timezone extends Iso8601Type {
   /**
